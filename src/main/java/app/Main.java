b5892e4d8ef900c7d70971bddb890ff6a9194a37
Juan Gomez Castillo <juan.gomezc@sofka.com.co>
2022-02-03 11:10:34 -0500

    change jpa for hibernate

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -6,6 +6,8 @@ import app.entity.Producto;
 import app.entity.Proveedore;
 import app.repository.ProductoRepository;
 import app.service.ProductoService;
+import app.utils.HibernateUtil;
+import org.hibernate.Session;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -14,15 +16,15 @@ import javax.persistence.Persistence;
 
 public class Main {
     public static void main(String[] args) {
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpaexample");
-        EntityManager em = emf.createEntityManager();
-        em.getTransaction().begin();
-        ProductoController productoController = new ProductoController(em);
-        ClienteController clienteController = new ClienteController(em);
-        ProveedoreController proveedoreController = new ProveedoreController(em);
-        CategoriaController categoriaController = new CategoriaController(em);
-        FacturaController facturaController = new FacturaController(em);
-        VentaController ventaController = new VentaController(em);
+        HibernateUtil.buildSessionFactory();
+        Session session = HibernateUtil.getCurrentSession().getSession();
+
+        ProductoController productoController = new ProductoController(session);
+        ClienteController clienteController = new ClienteController(session);
+        ProveedoreController proveedoreController = new ProveedoreController(session);
+        CategoriaController categoriaController = new CategoriaController(session);
+        FacturaController facturaController = new FacturaController(session);
+        VentaController ventaController = new VentaController(session);
 
         System.out.println(productoController.getProductAll());
         System.out.println("-----------------------------------");
